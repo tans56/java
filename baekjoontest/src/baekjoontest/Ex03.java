7bcc2c40e1db848b5f7ab2d7271eca87160f17c9
chanhee0506 <chyoo98@naver.com>
2023-03-15 19:14:19 +0900

    23/03/15

--- baekjoontest/src/baekjoontest/Ex03.java
+++ baekjoontest/src/baekjoontest/Ex03.java
@@ -1,30 +1,41 @@
 package baekjoontest;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 
 
 public class Ex03 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());	
-		String snum = br.readLine().replaceAll(" ","");	
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int v = Integer.parseInt(br.readLine());	
-		int[] arr = new int[n];		
-		int count = 0;
 		
-		for(int i =0; i<arr.length; i++) {			
-			
-			arr[i] += snum.charAt(i)-48;			
-			if(arr[i] == v) {
-				count++;
-			}	
+		int count = 0;
+		while(st.hasMoreTokens()) {
+			int t = Integer.parseInt(st.nextToken());
+			if(t == v) count++;
 		}
-		System.out.println(count);
 
-
+		bw.write(count + "\n");
+		bw.flush();
+//		int[] arr = new int[n];		
+//		int count = 0;
+//		
+//		for(int i =0; i<arr.length; i++) {			
+//			
+//			arr[i] += snum.charAt(i)-48;			
+//			if(arr[i] == v) {
+//				count++;
+//			}	
+//		}
+//		System.out.println(count);
 	}
 }
 
